removed KL-regularization because it was falsely implemnted + fixed LwF loss + started integration of SI

lib/Training/loss_functions.py
@@ -14,6 +14,31 @@ def get_kl(m, v, m0, v0):
     return (constTerm + logStdDiff + muDiffTerm) / torch.numel(m)
 
 
+# def loss_fn_kd(scores, target_scores, T=2.):
+#     """Compute knowledge-distillation (KD) loss given [scores] and [target_scores].
+#     Both [scores] and [target_scores] should be tensors, although [target_scores] should be repackaged.
+#     'Hyperparameter': temperature"""
+    
+#     #print("score", scores.shape)
+#     #print("target", target_scores.shape)
+
+#     log_scores_norm = F.log_softmax(scores / T, dim=1)
+#     log_scores_norm = log_scores_norm[:,:target_scores.shape[1]] # log_scores_norm[:,:target_scores.shape[1],:,:]
+#     targets_norm = F.softmax(target_scores / T, dim=1)
+#     #targets_norm = targets_norm[:,:,:,:]
+
+#     # Calculate distillation loss (see e.g., Li and Hoiem, 2017)
+#     KD_loss_unnorm = -(targets_norm * log_scores_norm)
+#     #print(KD_loss_unnorm.shape)
+#     KD_loss_unnorm = KD_loss_unnorm.sum(dim=1)                      #--> sum over classes
+#     #print(KD_loss_unnorm.shape)
+#     KD_loss_unnorm = KD_loss_unnorm.mean()                          #--> average over batch
+
+#     # normalize
+#     KD_loss = KD_loss_unnorm * T**2
+#     return KD_loss
+
+
 def loss_fn_kd(scores, target_scores, T=2.):
     """Compute knowledge-distillation (KD) loss given [scores] and [target_scores].
     Both [scores] and [target_scores] should be tensors, although [target_scores] should be repackaged.
@@ -22,10 +47,19 @@ def loss_fn_kd(scores, target_scores, T=2.):
     #print("score", scores.shape)
     #print("target", target_scores.shape)
 
+    device = scores.device
+
     log_scores_norm = F.log_softmax(scores / T, dim=1)
-    log_scores_norm = log_scores_norm[:,:target_scores.shape[1]] # log_scores_norm[:,:target_scores.shape[1],:,:]
     targets_norm = F.softmax(target_scores / T, dim=1)
-    #targets_norm = targets_norm[:,:,:,:]
+
+    # if [scores] and [target_scores] do not have equal size, append 0's to [targets_norm]
+    n = scores.size(1)
+    if n > target_scores.size(1):
+        n_batch = scores.size(0)
+        zeros_to_add = torch.zeros(n_batch, n-target_scores.size(1))
+        zeros_to_add = zeros_to_add.to(device)
+        targets_norm = torch.cat([targets_norm.detach(), zeros_to_add], dim=1)
+
 
     # Calculate distillation loss (see e.g., Li and Hoiem, 2017)
     KD_loss_unnorm = -(targets_norm * log_scores_norm)

lib/Training/train.py
@@ -4,6 +4,7 @@ from lib.Utility.metrics import AverageMeter
 from lib.Utility.metrics import accuracy
 from lib.Training.loss_functions import loss_fn_kd
 from lib.Utility.visualization import visualize_image_grid
+import lib.Models.si as SI
 
 
 def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device, args, save_path):
@@ -28,6 +29,7 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
     kld_losses = AverageMeter()
     losses = AverageMeter()
     lwf_losses = AverageMeter()
+    si_losses = AverageMeter()
     batch_time = AverageMeter()
     data_time = AverageMeter()
 
@@ -73,12 +75,12 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
             recon_target = (recon_target * 255).long()
 
         # calculate loss
-        if args.use_kl_regularization:
-            class_loss, recon_loss, kld_loss = criterion(class_samples, class_target, recon_samples, recon_target, mu, std,
-                                                        model.module.prev_mu, model.module.prev_std, device, args)
-        else:
-            class_loss, recon_loss, kld_loss = criterion(class_samples, class_target, recon_samples, recon_target, mu, std,
-                                                        device, args)
+        #if args.use_kl_regularization:
+        #    class_loss, recon_loss, kld_loss = criterion(class_samples, class_target, recon_samples, recon_target, mu, std,
+        #                                                model.module.prev_mu, model.module.prev_std, device, args)
+        #else:
+        class_loss, recon_loss, kld_loss = criterion(class_samples, class_target, recon_samples, recon_target, mu, std,
+                                                    device, args)
 
         # add the individual loss components together and weight the KL term.
         loss = class_loss + recon_loss + args.var_beta * kld_loss
@@ -90,7 +92,7 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
             prev_cl_losses = torch.zeros(prev_pred_class_samples.size(0)).to(device)
             # loop through each sample for each input and calculate the correspond loss. Normalize the losses.
             for s in range(prev_pred_class_samples.size(0)):
-                prev_cl_losses[s] = loss_fn_kd(class_samples[s], prev_pred_class_samples[s]) / torch.numel(target)
+                prev_cl_losses[s] = loss_fn_kd(class_samples[s], prev_pred_class_samples[s]) #/ torch.numel(target)
             
             # average the loss over all samples per input
             cl_lwf = torch.mean(prev_cl_losses, dim=0)
@@ -100,6 +102,14 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
 
             # record lwf losses
             lwf_losses.update(cl_lwf.item(), inp.size(0))
+
+        # calculate SI loss (if SI is initialized)
+        if args.use_si and model.module.si_storage.is_initialized:
+            loss_si = args.lmda * SI.surrogate_loss(model.module, model.module.si_storage)
+            print(loss_si.item())
+            loss += loss_si
+
+            si_losses.update(loss_si.item(), inp.size(0))
 
         # take mean to compute accuracy. Note if variational samples are 1 this only gets rid of a dummy dimension.
         output = torch.mean(class_samples, dim=0)
@@ -116,6 +126,11 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        # SI: update running si paramters
+        if args.use_si:
+            SI.update_si_parameters(model.module, model.module.si_storage)
+            #print("SI: Updated running parameters")
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -136,6 +151,20 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
                     epoch, i, len(Dataset.train_loader), batch_time=batch_time,
                     data_time=data_time, loss=losses, cl_loss=class_losses, top1=top1,
                     recon_loss=recon_losses, KLD_loss=kld_losses, lwf_loss=cl_lwf.item()))
+            if args.use_si and model.module.si_storage.is_initialized:
+                print('Training: [{0}][{1}/{2}]\t' 
+                    'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                    'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                    'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                    'Class Loss {cl_loss.val:.4f} ({cl_loss.avg:.4f})\t'
+                    'SI Loss {si_loss:.4f}\t'
+                    'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                    'Recon Loss {recon_loss.val:.4f} ({recon_loss.avg:.4f})\t'
+                    'KL {KLD_loss.val:.4f} ({KLD_loss.avg:.4f})'.format(
+                    epoch, i, len(Dataset.train_loader), batch_time=batch_time,
+                    data_time=data_time, loss=losses, cl_loss=class_losses, top1=top1,
+                    recon_loss=recon_losses, KLD_loss=kld_losses, si_loss=loss_si.item()))
+            
             else:
                 print('Training: [{0}][{1}/{2}]\t' 
                     'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
@@ -167,6 +196,8 @@ def train(Dataset, model, criterion, epoch, iteration, optimizer, writer, device
     
     if args.use_lwf:
         writer.add_scalar('training/train_lwf_loss', lwf_losses.avg, iteration[0])
+    if args.use_si:
+        writer.add_scalar('training/train_si_loss', si_losses.avg, iteration[0])
 
     # If the log weights argument is specified also add parameter and gradient histograms to TensorBoard.
     if args.log_weights:

main.py
@@ -31,7 +31,7 @@ from lib.Training.evaluate import get_mu_and_std
 from lib.Utility.visualization import args_to_tensorboard
 from lib.Utility.visualization import visualize_dataset_in_2d_embedding
 from lib.Models.architectures import set_previous_mu_and_std
-
+import lib.Models.si as SI
 
 # Comment this if CUDNN benchmarking is not desired
 cudnn.benchmark = True
@@ -67,8 +67,10 @@ def main():
             save_path += '_genreplay'
         if args.openset_generative_replay:
             save_path += '_opensetreplay'
-        if args.use_kl_regularization:
-            save_path += '_kl-reg'
+        #if args.use_kl_regularization:
+        #    save_path += '_kl-reg'
+        if args.use_si:
+            save_path += '_si'
         if args.use_lwf:
             save_path += '_lwf'
     if args.cross_dataset:
@@ -86,10 +88,10 @@ def main():
         log.write(arg + ':' + str(getattr(args, arg)) + '\n')
 
     # Initialize criterion
-    if args.use_kl_regularization:
-        from lib.Training.loss_functions import unified_loss_function_kl_regularized as criterion
-    else:
-        from lib.Training.loss_functions import unified_loss_function as criterion
+    #if args.use_kl_regularization:
+    #    from lib.Training.loss_functions import unified_loss_function_kl_regularized as criterion
+    #else:
+    from lib.Training.loss_functions import unified_loss_function as criterion
 
 
     # Dataset loading
@@ -258,6 +260,11 @@ def main():
     WeightInitializer = WeightInit(args.weight_init)
     WeightInitializer.init_model(model)
 
+    # SI: Register all model paramters
+    if args.use_si:
+        SI.register_si_params(model.module, model.module.si_storage)
+        print("SI: Initial paramters got registered")
+
     # Define optimizer and loss function (criterion)
     optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
 
@@ -281,6 +288,10 @@ def main():
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
+    # SI: Prepare storing of running parameter updates for this sequence (reset dicts W and p_old)
+    SI.init_si_params(model.module, model.module.si_storage)
+    print("SI: Reset running paramters for next task")
+    
     # optimize until final amount of epochs is reached. Final amount of epochs is determined through the
     while epoch < (args.epochs * epoch_multiplier):
         print("")
@@ -309,16 +320,23 @@ def main():
                 save_task_checkpoint(save_path, epoch // args.epochs)
 
                 # track mu and std for regularization
-                if args.use_kl_regularization:
-                    print("Calculating task's mu and std for kl regularization")
-                    prev_mu, prev_std = get_mu_and_std(model, dataset.train_loader, device)
-                    set_previous_mu_and_std(model, prev_mu, prev_std)
+                #if args.use_kl_regularization:
+                #    print("Calculating task's mu and std for kl regularization")
+                #    prev_mu, prev_std = get_mu_and_std(model, dataset.train_loader, device)
+                #    set_previous_mu_and_std(model, prev_mu, prev_std)
                 
                 # save previous model if lwf
                 if args.use_lwf:
                     print("Storing previous model")
                     model.module.prev_model = copy.deepcopy(model)
                     print("Storing complete...")
+
+                # perform SI calculations
+                if args.use_si:
+                    # SI: Update internal paramters
+                    SI.update_si_integral(model.module, model.module.si_storage)
+                    print("SI: Updated Omega")
+                    
 
                 print("Incrementing dataset...")
                 dataset.increment_tasks(model, args.batch_size, args.workers, writer, save_path,
@@ -346,6 +364,14 @@ def main():
                     model.module.num_classes += args.num_increment_tasks
                     grow_classifier(device, model.module.classifier, args.num_increment_tasks, WeightInitializer)
                     print("Classifier grown..")
+
+                    # Register new paramters to SI
+                    if args.use_si:
+                        SI.update_registered_si_params(model.module, model.module.si_storage)
+                        print("SI: Updated registration of SI params for grown model")
+                        # SI: Reset running parameters (after growing)
+                        SI.init_si_params(model.module, model.module.si_storage)
+                        print("SI: Reset running paramters for next task")
 
                 # reset moving averages etc. of the optimizer
                 optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)