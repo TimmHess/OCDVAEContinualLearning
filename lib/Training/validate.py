6ac6af0abe661920d3d9561ad528ea25fc29f6d9
Timm Hess <timm.hess@hotmail.de>
2020-09-14 20:02:33 +0200

    bug fix: single-headed non-vae validation

--- lib/Training/validate.py
+++ lib/Training/validate.py
@@ -225,100 +225,6 @@ def validate(Dataset, model, criterion, epoch, iteration, writer, device, save_p
                             top1=top1, recon_loss=recon_losses_nat, KLD_loss=kld_losses))
 
 
-        # elif args.is_multiheaded and args.incremental_instance:
-        #     # created dataset loader for each validation set in mh_valsets(ordered by task/head)
-        #     for valset_index in range(len(Dataset.mh_valsets)):
-        #         val_loader = torch.utils.data.DataLoader(Dataset.mh_valsets[valset_index], batch_size=args.batch_size, shuffle=False,
-        #                                              num_workers=args.workers, pin_memory=torch.cuda.is_available(), drop_last=True)
-        #         # iterate validation set
-        #         for i, (inp, target) in enumerate(val_loader):
-        #             inp = inp.to(device)
-        #             target = target.to(device)
-
-        #             recon_target = inp
-        #             class_target = target
-
-        #             # visualize inp
-        #             if epoch % args.epochs == 0 and i == 0:
-        #                 visualize_image_grid(inp, writer, epoch + 1, 'val_inp_snapshot', save_path)
-
-        #             # compute output
-        #             class_samples, recon_samples, mu, std = model(inp) # class_samples are (global) target space
-
-        #             # computer loss for respective head
-        #             head_start = (valset_index) * Dataset.num_classes 
-        #             head_end = (valset_index+1) * Dataset.num_classes
-        #             print("head start, end:", head_start, head_end)
-        #             class_loss, recon_loss, kld_loss = criterion(class_samples[:,:,head_start:head_end], class_target, recon_samples, recon_target, mu, std,
-        #                                                     device, args)
-
-        #             # take mean to compute accuracy
-        #             # (does nothing if there isn't more than 1 sample per input other than removing dummy dimension)
-        #             class_output = torch.mean(class_samples, dim=0)
-        #             if not args.no_vae:
-        #                 recon_output = torch.mean(recon_samples, dim=0)
-
-        #             # convert model outputs back to (global) target space to map back to known evaluation
-        #             # copy only current head output back into an zero-ed tensor
-        #             task_class_output = torch.zeros_like(class_output).fill_(-100000)
-        #             task_class_output[:,head_start:head_end] = class_output[:,head_start:head_end]
-                
-        #             # measure accuracy, record loss, fill confusion matrix
-        #             prec1 = accuracy(task_class_output, target)[0]
-        #             top1.update(prec1.item(), inp.size(0))
-        #             #confusion.add(class_output.data, target)
-        #             confusion.add(task_class_output.data, target)
-
-        #             # measure elapsed time
-        #             batch_time.update(time.time() - end)
-        #             end = time.time()
-
-        #             # skipped code for autoregressive model
-
-        #             # If not autoregressive simply apply the Sigmoid and visualize
-        #             if not args.no_vae:
-        #                 recon = torch.sigmoid(recon_output)
-        #                 if (i == (len(Dataset.val_loader) - 2)) and (epoch % args.visualization_epoch == 0):
-        #                     visualize_image_grid(recon, writer, epoch + 1, 'reconstruction_snapshot', save_path)
-                    
-        #             # update the respective loss values. To be consistent with values reported in the literature we scale
-        #             # our normalized losses back to un-normalized values.
-        #             # For the KLD this also means the reported loss is not scaled by beta, to allow for a fair comparison
-        #             # across potential weighting terms.
-        #             class_losses.update(class_loss.item() * model.module.num_classes, inp.size(0))
-        #             if args.no_vae:
-        #                 losses.update(class_loss.item(), inp.size(0))
-        #             else:
-        #                 kld_losses.update(kld_loss.item() * model.module.latent_dim, inp.size(0))
-        #                 recon_losses_nat.update(recon_loss.item() * inp.size()[1:].numel(), inp.size(0))
-        #                 losses.update((class_loss + recon_loss + kld_loss).item(), inp.size(0))
-
-
-        #             # If we are at the end of validation, create one mini-batch of example generations. Only do this every
-        #             # other epoch specified by visualization_epoch to avoid generation of lots of images and computationally
-        #             # expensive calculations of the autoregressive model's generation.
-        #             if not args.no_vae:
-        #                 if i == (len(Dataset.val_loader) - 2) and epoch % args.visualization_epoch == 0:
-        #                     # generation
-        #                     gen = model.module.generate()
-
-        #                     if args.autoregression:
-        #                         gen = model.module.pixelcnn.generate(gen)
-        #                     visualize_image_grid(gen, writer, epoch + 1, 'generation_snapshot', save_path)
-
-        #             # Print progress
-        #             if i % args.print_freq == 0:
-        #                 print('Validate: [{0}][{1}/{2}]\t' 
-        #                     'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t' 
-        #                     'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-        #                     'Class Loss {cl_loss.val:.4f} ({cl_loss.avg:.4f})\t'
-        #                     'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-        #                     'Recon Loss {recon_loss.val:.4f} ({recon_loss.avg:.4f})\t'
-        #                     'KL {KLD_loss.val:.4f} ({KLD_loss.avg:.4f})'.format(
-        #                     epoch+1, i, len(Dataset.val_loader), batch_time=batch_time, loss=losses, cl_loss=class_losses,
-        #                     top1=top1, recon_loss=recon_losses_nat, KLD_loss=kld_losses))
-
-
         else:
             for i, (inp, target) in enumerate(Dataset.val_loader):
                 inp = inp.to(device)
@@ -356,7 +262,8 @@ def validate(Dataset, model, criterion, epoch, iteration, writer, device, save_p
                 # take mean to compute accuracy
                 # (does nothing if there isn't more than 1 sample per input other than removing dummy dimension)
                 class_output = torch.mean(class_samples, dim=0)
-                recon_output = torch.mean(recon_samples, dim=0)
+                if not args.no_vae:
+                    recon_output = torch.mean(recon_samples, dim=0)
 
                 # measure accuracy, record loss, fill confusion matrix
                 prec1 = accuracy(class_output, target)[0]
@@ -371,34 +278,36 @@ def validate(Dataset, model, criterion, epoch, iteration, writer, device, save_p
                 # multinomial distribution (Reminder: the original output is a 255 way Softmax as PixelVAEs are posed as a
                 # classification problem). This serves two purposes: visualization of reconstructions and computation of
                 # a reconstruction loss in nats using a BCE loss, comparable to that of a regular VAE.
-                recon_target = inp
-                if args.autoregression:
-                    recon = torch.zeros((inp.size(0), inp.size(1), inp.size(2), inp.size(3))).to(device)
-                    for h in range(inp.size(2)):
-                        for w in range(inp.size(3)):
-                            for c in range(inp.size(1)):
-                                probs = torch.softmax(recon_output[:, :, c, h, w], dim=1).data
-                                pixel_sample = torch.multinomial(probs, 1).float() / 255.
-                                recon[:, c, h, w] = pixel_sample.squeeze()
+                if not args.no_vae:
+                    recon_target = inp
+                    if args.autoregression:
+                        recon = torch.zeros((inp.size(0), inp.size(1), inp.size(2), inp.size(3))).to(device)
+                        for h in range(inp.size(2)):
+                            for w in range(inp.size(3)):
+                                for c in range(inp.size(1)):
+                                    probs = torch.softmax(recon_output[:, :, c, h, w], dim=1).data
+                                    pixel_sample = torch.multinomial(probs, 1).float() / 255.
+                                    recon[:, c, h, w] = pixel_sample.squeeze()
 
-                    if (epoch % args.visualization_epoch == 0) and (i == (len(Dataset.val_loader) - 2)):
-                        visualize_image_grid(recon, writer, epoch + 1, 'reconstruction_snapshot', save_path)
+                        if (epoch % args.visualization_epoch == 0) and (i == (len(Dataset.val_loader) - 2)):
+                            visualize_image_grid(recon, writer, epoch + 1, 'reconstruction_snapshot', save_path)
 
-                    recon_loss = F.binary_cross_entropy(recon, recon_target)
-                else:
-                    # If not autoregressive simply apply the Sigmoid and visualize
-                    recon = torch.sigmoid(recon_output)
-                    if (i == (len(Dataset.val_loader) - 2)) and (epoch % args.visualization_epoch == 0):
-                        visualize_image_grid(recon, writer, epoch + 1, 'reconstruction_snapshot', save_path)
+                        recon_loss = F.binary_cross_entropy(recon, recon_target)
+                    else:
+                        # If not autoregressive simply apply the Sigmoid and visualize
+                        recon = torch.sigmoid(recon_output)
+                        if (i == (len(Dataset.val_loader) - 2)) and (epoch % args.visualization_epoch == 0):
+                            visualize_image_grid(recon, writer, epoch + 1, 'reconstruction_snapshot', save_path)
 
                 # update the respective loss values. To be consistent with values reported in the literature we scale
                 # our normalized losses back to un-normalized values.
                 # For the KLD this also means the reported loss is not scaled by beta, to allow for a fair comparison
                 # across potential weighting terms.
                 class_losses.update(class_loss.item() * model.module.num_classes, inp.size(0))
-                kld_losses.update(kld_loss.item() * model.module.latent_dim, inp.size(0))
-                recon_losses_nat.update(recon_loss.item() * inp.size()[1:].numel(), inp.size(0))
-                losses.update((class_loss + recon_loss + kld_loss).item(), inp.size(0))
+                if not args.no_vae:
+                    kld_losses.update(kld_loss.item() * model.module.latent_dim, inp.size(0))
+                    recon_losses_nat.update(recon_loss.item() * inp.size()[1:].numel(), inp.size(0))
+                    losses.update((class_loss + recon_loss + kld_loss).item(), inp.size(0))
 
                 # if we are learning continually, we need to calculate the base and new reconstruction losses at the end
                 # of each task increment.
@@ -421,28 +330,30 @@ def validate(Dataset, model, criterion, epoch, iteration, writer, device, save_p
                                                         recon_target.size(3))
 
                         # If the input belongs to one of the base classes also update base metrics
-                        if class_target[j].item() in base_classes:
-                            if args.autoregression:
-                                recon_losses_base_bits_per_dim.update(F.cross_entropy(rec, (rec_tar * 255).long()) *
-                                                                    math.log2(math.e), 1)
-                            recon_losses_base_nat.update(F.binary_cross_entropy(recon[j], recon_target[j]), 1)
-                        # if the input belongs to one of the new classes also update new metrics
-                        elif class_target[j].item() in new_classes:
-                            if args.autoregression:
-                                recon_losses_new_bits_per_dim.update(F.cross_entropy(rec, (rec_tar * 255).long()) *
-                                                                    math.log2(math.e), 1)
-                            recon_losses_new_nat.update(F.binary_cross_entropy(recon[j], recon_target[j]), 1)
+                        if not args.no_vae:
+                            if class_target[j].item() in base_classes:
+                                if args.autoregression:
+                                    recon_losses_base_bits_per_dim.update(F.cross_entropy(rec, (rec_tar * 255).long()) *
+                                                                        math.log2(math.e), 1)
+                                recon_losses_base_nat.update(F.binary_cross_entropy(recon[j], recon_target[j]), 1)
+                            # if the input belongs to one of the new classes also update new metrics
+                            elif class_target[j].item() in new_classes:
+                                if args.autoregression:
+                                    recon_losses_new_bits_per_dim.update(F.cross_entropy(rec, (rec_tar * 255).long()) *
+                                                                        math.log2(math.e), 1)
+                                recon_losses_new_nat.update(F.binary_cross_entropy(recon[j], recon_target[j]), 1)
 
                 # If we are at the end of validation, create one mini-batch of example generations. Only do this every
                 # other epoch specified by visualization_epoch to avoid generation of lots of images and computationally
                 # expensive calculations of the autoregressive model's generation.
-                if i == (len(Dataset.val_loader) - 2) and epoch % args.visualization_epoch == 0:
-                    # generation
-                    gen = model.module.generate()
+                if not args.no_vae:
+                    if i == (len(Dataset.val_loader) - 2) and epoch % args.visualization_epoch == 0:
+                        # generation
+                        gen = model.module.generate()
 
-                    if args.autoregression:
-                        gen = model.module.pixelcnn.generate(gen)
-                    visualize_image_grid(gen, writer, epoch + 1, 'generation_snapshot', save_path)
+                        if args.autoregression:
+                            gen = model.module.pixelcnn.generate(gen)
+                        visualize_image_grid(gen, writer, epoch + 1, 'generation_snapshot', save_path)
 
                 # Print progress
                 if i % args.print_freq == 0:
